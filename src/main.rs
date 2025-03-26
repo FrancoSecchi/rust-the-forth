19bda656da735d2efb187213854f56a65be0c4dc
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-26 17:10:40 -0300

    split numnero y push al stack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut number_stack: Vec<i16> = vec![];
+    let string: &str = "1 2 3";
+
+    for number_str in string.split_whitespace() {        
+        match number_str.parse::<i16>() {
+            Ok(number) => number_stack.push(number),
+            Err(_) => println!("No se pudo convertir: {}", number_str),
+        }
+    }
+    
+    // Imprimir los números
+    for number in number_stack {
+        println!("{number}");
+    }
 }
