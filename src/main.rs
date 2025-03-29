99247628f86cd9cd656017a591a28b6f82eb4a14
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-29 17:49:04 -0300

    Lectura de archivo

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-fn main() {
-    let mut number_stack: Vec<i16> = vec![];
-    let string: &str = "1 2 +";
+use std::fs::File;
+use std::env;
+use std::io::Read;
 
-    for token in string.split_whitespace() {        
-        match token.parse::<i16>() {
-            Ok(token) => number_stack.push(token),
-            Err(_) => {
-                if token.len() == 1 {
-                    if token == "+" {
-                        if let (Some(a_val), Some(b_val)) = (number_stack.pop(), number_stack.pop()) {
-                            number_stack.push(b_val + a_val); 
-                        }                     
-                    }
-                }
-            },
-        }
+
+fn open_file(path: &String) {
+    println!("El path es: {path}");
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(why) => panic!("No se puede abrir el archivo {}", why)
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("No se puede abrir el archivo {}", why),
+        Ok(_) => println!("{}", s)
     }
-    
-    // Imprimir los números
-    for number in number_stack {
-        println!("{number}");
-    }
+
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    open_file(path);
 }
