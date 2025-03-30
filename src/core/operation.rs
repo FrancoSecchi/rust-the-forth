f242705415ed97853f15c4fdb40ce50b4e54a894
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-30 17:36:02 -0300

    Implementación division

--- src/core/operation.rs
+++ src/core/operation.rs
@@ -42,10 +42,26 @@ impl Operation for Mul {
     }
 }
 
+#[derive(Debug)]
+pub struct Div;
+
+impl Operation for Div {
+    fn apply(&self, stack: &mut Vec<i16>) -> Result<(), OperationError> {
+        let a = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        let b = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        if b == 0 {
+            return Err(OperationError::DivisionByZero)
+        }
+        stack.push(a / b);
+        Ok(())
+    }
+}
+
 pub fn get_operations() -> HashMap<String, Box<dyn Operation>> {
     let mut ops = HashMap::new();
     ops.insert("+".to_string(), Box::new(Add) as Box<dyn Operation>);
     ops.insert("-".to_string(), Box::new(Sub) as Box<dyn Operation>);
     ops.insert("*".to_string(), Box::new(Mul) as Box<dyn Operation>);
+    ops.insert("/".to_string(), Box::new(Div) as Box<dyn Operation>);
     ops
 }
