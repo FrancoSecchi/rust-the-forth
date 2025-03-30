55f4bd97e93866045b98373576baecdff86644f4
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-30 16:11:11 -0300

    struct errores + display

---
+++ src/core/error.rs
@@ -0,0 +1,18 @@
+use std::fmt::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum OperationError {
+    StackUnderflow,
+    DivisionByZero,
+}
+
+
+impl fmt::Display for OperationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            OperationError::StackUnderflow => write!(f, "Stack underflow"),
+            OperationError::DivisionByZero => write!(f, "Division by zero"),
+        }
+    }
+}
