912761c926ad75124faf9449d0a4dd1ba5f33a1f
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-30 17:15:40 -0300

    fix get token

--- src/core/operation.rs
+++ src/core/operation.rs
@@ -18,8 +18,21 @@ impl Operation for Add {
     }
 }
 
+#[derive(Debug)]
+pub struct Sub;
+
+impl Operation for Sub {
+    fn apply(&self, stack: &mut Vec<i16>) -> Result<(), OperationError> {
+        let a = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        let b = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        stack.push(a - b);
+        Ok(())
+    }
+}
+
 pub fn get_operations() -> HashMap<String, Box<dyn Operation>> {
     let mut ops = HashMap::new();
     ops.insert("+".to_string(), Box::new(Add) as Box<dyn Operation>);
+    ops.insert("-".to_string(), Box::new(Sub) as Box<dyn Operation>);
     ops
 }
