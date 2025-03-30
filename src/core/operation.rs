ee3361f0c076963f78d80ceaf310f580e496e829
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-30 17:18:47 -0300

    Implementacion mul

--- src/core/operation.rs
+++ src/core/operation.rs
@@ -30,9 +30,22 @@ impl Operation for Sub {
     }
 }
 
+#[derive(Debug)]
+pub struct Mul;
+
+impl Operation for Mul {
+    fn apply(&self, stack: &mut Vec<i16>) -> Result<(), OperationError> {
+        let a = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        let b = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        stack.push(a * b);
+        Ok(())
+    }
+}
+
 pub fn get_operations() -> HashMap<String, Box<dyn Operation>> {
     let mut ops = HashMap::new();
     ops.insert("+".to_string(), Box::new(Add) as Box<dyn Operation>);
     ops.insert("-".to_string(), Box::new(Sub) as Box<dyn Operation>);
+    ops.insert("*".to_string(), Box::new(Mul) as Box<dyn Operation>);
     ops
 }
