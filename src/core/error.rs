7fdf917417e89cc63ae2fab4ebca1f9eae7fe1e8
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 15:06:47 -0300

    errors comentarios

--- src/core/error.rs
+++ src/core/error.rs
@@ -1,18 +1,30 @@
 use std::fmt;
 
+/// Represents possible errors that can occur during operation execution.
 #[derive(Debug)]
 pub enum OperationError {
+    /// The stack does not contain enough elements to perform the operation.
     StackUnderflow,
+    /// The stack has reached its maximum capacity.
     StackOverflow,
+    /// Attempted to divide by zero.
     DivisionByZero,
+    /// An invalid word was encountered.
     InvalidWord,
+    /// Failed to write the stack to the file.
     FailWritingFile,
+    /// The output string is null or empty.
     StringNull,
+    /// The character provided for the `emit` operation is invalid.
     InvalidCharacter,
+    /// The provided word was not recognized as a valid operation.
     WordNotFound,
 }
 
 impl fmt::Display for OperationError {
+    /// Formats the error message for display.
+    ///
+    /// Each variant of `OperationError` is converted into a user-friendly message.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             OperationError::StackUnderflow => writeln!(f, "stack-underflow"),
