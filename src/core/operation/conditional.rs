cc44dc2868fe89a682996811c2fa90daff566733
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 01:05:31 -0300

    greater tests

--- src/core/operation/conditional.rs
+++ src/core/operation/conditional.rs
@@ -72,4 +72,41 @@ mod tests {
             ));
         }
     }
+
+    mod grater_tests {
+        use super::*;
+        #[test]
+        fn test_greater_numbers() {
+            let mut stack: Vec<i16> = vec![2, 1];
+            Greater.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![-1]);
+        }
+        #[test]
+        fn test_not_greater_number() {
+            let mut stack: Vec<i16> = vec![2, 3];
+            Greater.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![0]);
+        }
+        #[test]
+        fn test_greater_numbers_many_elements() {
+            let mut stack: Vec<i16> = vec![2, 4, 3];
+            Greater.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![2, -1]);
+        }
+
+        #[test]
+        fn test_underflow_greater() {
+            let mut stack: Vec<i16> = vec![];
+            assert!(matches!(
+                Greater.apply(&mut stack),
+                Err(OperationError::StackUnderflow)
+            ));
+            
+            let mut second_stack: Vec<i16> = vec![1];
+            assert!(matches!(
+                Greater.apply(&mut second_stack),
+                Err(OperationError::StackUnderflow)
+            ));
+        }
+    }
 }
