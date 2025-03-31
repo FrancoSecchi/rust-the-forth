dc24afe53001a8e5a1aff91ed57502f39fc8e7d2
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 17:31:24 -0300

    get operations para stack manipulation

--- src/core/operation/stack_manipulation/mod.rs
+++ src/core/operation/stack_manipulation/mod.rs
@@ -4,8 +4,26 @@ pub mod over;
 pub mod rot;
 pub mod swap;
 
+use std::collections::HashMap;
+
 pub use drop::Drop;
 pub use dup::Dup;
 pub use over::Over;
 pub use rot::Rot;
 pub use swap::Swap;
+
+use super::{Operation, OperationType};
+
+
+pub fn get_operations() -> HashMap<OperationType, Box<dyn Operation>> {
+    let mut ops = HashMap::new();
+    ops.insert(OperationType::Drop, Box::new(Drop) as Box<dyn Operation>);
+    ops.insert(OperationType::Dup, Box::new(Dup) as Box<dyn Operation>);
+    ops.insert(
+        OperationType::Over,
+        Box::new(Over) as Box<dyn Operation>,
+    );
+    ops.insert(OperationType::Rot, Box::new(Rot) as Box<dyn Operation>);
+    ops.insert(OperationType::Swap, Box::new(Swap) as Box<dyn Operation>);
+    ops
+}
