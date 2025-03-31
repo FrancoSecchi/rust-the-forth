bd5ae32e49289865e3b6b1f44d4687908e8797d8
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 02:12:56 -0300

    errores

--- src/core/error.rs
+++ src/core/error.rs
@@ -3,14 +3,18 @@ use std::fmt;
 #[derive(Debug)]
 pub enum OperationError {
     StackUnderflow,
+    StackOverflow,
     DivisionByZero,
+    InvalidWord,
 }
 
 impl fmt::Display for OperationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             OperationError::StackUnderflow => writeln!(f, "stack-underflow"),
-            OperationError::DivisionByZero => writeln!(f, "division-by-zero"),
+            OperationError::StackOverflow => writeln!(f, "stack-overflow"),
+            OperationError::DivisionByZero => writeln!(f, "division-by-zero"),            
+            OperationError::InvalidWord => writeln!(f, "invalid-word"),            
         }
     }
 }
