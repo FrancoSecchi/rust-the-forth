d15a6f11d94634843b1c2e18a261e0821107fe20
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 01:24:20 -0300

    and tests

--- src/core/operation/boolean.rs
+++ src/core/operation/boolean.rs
@@ -55,7 +55,7 @@ impl Operation for And {
             return Err(OperationError::StackUnderflow);
         }
         let item = stack.pop().ok_or(OperationError::StackUnderflow)?;
-        let second_item = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        let second_item = stack.pop().ok_or(OperationError::StackUnderflow)?;        
         let result: i16 = if second_item != 0 && item != 0 {-1} else {0};        
         stack.push(result);        
         Ok(())
@@ -174,4 +174,45 @@ mod tests {
             ));
         }
     }
+    
+    mod and_tests {
+        use super::*;
+        #[test]
+        fn test_and_numbers() {
+            let mut stack: Vec<i16> = vec![-1, -1];
+            And.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![-1]);
+        }
+        #[test]
+        fn test_not_and_number() {
+            let mut stack: Vec<i16> = vec![-1, 0];
+            And.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![0]);
+            let mut stack_two: Vec<i16> = vec![0, 0];        
+            And.apply(&mut stack_two).unwrap();
+            assert_eq!(stack_two, vec![0]);
+        }
+        #[test]
+        fn test_and_numbers_many_elements() {
+            let mut stack: Vec<i16> = vec![2, -1, -1];
+            And.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![2, -1]);
+        }
+
+        #[test]
+        fn test_underflow_and() {
+            let mut stack: Vec<i16> = vec![];
+            assert!(matches!(
+                And.apply(&mut stack),
+                Err(OperationError::StackUnderflow)
+            ));
+            
+            let mut second_stack: Vec<i16> = vec![1];
+            assert!(matches!(
+                And.apply(&mut second_stack),
+                Err(OperationError::StackUnderflow)
+            ));
+        }
+    }
+
 }
