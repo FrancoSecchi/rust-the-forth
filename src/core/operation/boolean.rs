4712570542b977eea5bd3d745c78789bc3346623
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 01:26:35 -0300

    or tests

--- src/core/operation/boolean.rs
+++ src/core/operation/boolean.rs
@@ -228,5 +228,44 @@ mod tests {
             ));
         }
     }
+    mod or_tests {
+        use super::*;
+        #[test]
+        fn test_or_numbers() {
+            let mut stack: Vec<i16> = vec![-1, -1];
+            Or.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![-1]);
+        }
+        #[test]
+        fn test_or_multiple_conditions() {
+            let mut stack: Vec<i16> = vec![-1, 0];
+            Or.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![-1]);
+            let mut stack_two: Vec<i16> = vec![0, 0];        
+            Or.apply(&mut stack_two).unwrap();
+            assert_eq!(stack_two, vec![0]);
+        }
+        #[test]
+        fn test_or_numbers_many_elements() {
+            let mut stack: Vec<i16> = vec![2, -1, -1];
+            Or.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![2, -1]);
+        }
+
+        #[test]
+        fn test_underflow_or() {
+            let mut stack: Vec<i16> = vec![];
+            assert!(matches!(
+                Or.apply(&mut stack),
+                Err(OperationError::StackUnderflow)
+            ));
+            
+            let mut second_stack: Vec<i16> = vec![1];
+            assert!(matches!(
+                Or.apply(&mut second_stack),
+                Err(OperationError::StackUnderflow)
+            ));
+        }
+    }
 
 }
