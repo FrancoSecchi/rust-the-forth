b1ee59879015e8b1b8bc65b27ccb554768467620
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 01:31:57 -0300

    not tests

--- src/core/operation/boolean.rs
+++ src/core/operation/boolean.rs
@@ -280,5 +280,37 @@ mod tests {
             ));
         }
     }
+    mod not_tests {
+        use super::*;
+        #[test]
+        fn test_not_numbers() {
+            let mut stack: Vec<i16> = vec![10];
+            Not.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![0]);
+        }
+        #[test]
+        fn test_not_multiple_times() {
+            let mut stack: Vec<i16> = vec![10];
+            Not.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![0]);
+            Not.apply(&mut stack).unwrap();        
+            assert_eq!(stack, vec![-1]);
+        }
+        #[test]
+        fn test_not_numbers_many_elements() {
+            let mut stack: Vec<i16> = vec![2, -1, -1];
+            Not.apply(&mut stack).unwrap();
+            assert_eq!(stack, vec![2, -1, 0]);
+        }
+
+        #[test]
+        fn test_underflow_not() {
+            let mut stack: Vec<i16> = vec![];
+            assert!(matches!(
+                Not.apply(&mut stack),
+                Err(OperationError::StackUnderflow)
+            ));
+        }
+    }
 
 }
