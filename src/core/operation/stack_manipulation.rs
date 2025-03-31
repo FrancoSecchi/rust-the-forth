7201fc523bb975e21889a96f21d465e605893876
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 00:08:24 -0300

    Dup implementacion

--- src/core/operation/stack_manipulation.rs
+++ src/core/operation/stack_manipulation.rs
@@ -1,1 +1,20 @@
+use crate::core::error::OperationError;
+use crate::core::operation::Operation;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Dup;
+
+impl Operation for Dup {
+    fn apply(&self, stack: &mut Vec<i16>) -> Result<(), OperationError> {
+        if stack.is_empty() {
+            return Err(OperationError::StackUnderflow);
+        }
+        let item = stack.pop().ok_or(OperationError::StackUnderflow)?;
+
+        stack.push(item);
+        stack.push(item);
+        Ok(())
+    }
+}
 
