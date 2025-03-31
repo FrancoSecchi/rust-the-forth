d4b3dc8882a779da6161d8b64f37d68ecbce57c8
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-31 00:17:45 -0300

    swap implementacion

--- src/core/operation/stack_manipulation.rs
+++ src/core/operation/stack_manipulation.rs
@@ -31,6 +31,26 @@ impl Operation for Drop {
     }
 }
 
+//DUP, DROP, SWAP, OVER, ROT
+
+
+#[derive(Debug)]
+pub struct Swap;
+
+impl Operation for Swap {
+    fn apply(&self, stack: &mut Vec<i16>) -> Result<(), OperationError> {
+        if stack.is_empty() {
+            return Err(OperationError::StackUnderflow);
+        }
+        let last_item = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        let penultimate_item = stack.pop().ok_or(OperationError::StackUnderflow)?;
+        stack.push(last_item);
+        stack.push(penultimate_item);
+        Ok(())
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
